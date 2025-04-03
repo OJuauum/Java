3b4658ad759a0c59221b9d87c09a84fab338b37d
João Paulo <joaopaulodesouza6@gmail.com>
2025-04-03 13:39:47 -0300

    commit

---
+++ OperadoresAritmeticos/src/main/java/operadoresaritmeticos/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package operadoresaritmeticos.operadoresaritmeticos;
+
+/**
+ *
+ * @author juba1
+ */
+public class OperadoresAritmeticos {
+
+    public static void main(String[] args) {
+        /*int n1 = 3;
+        int n2 = 5;
+        float m = (n1+n2)/2;
+        System.out.println("A media e igual a " + m);*/
+        
+        /*int numero = 5;
+        numero ++;
+        System.out.println(numero);
+        
+        int numer = 5;
+        numero --;
+        System.out.println(numero);*/
+        
+        /*int x = 4;
+        x += 2;
+        System.out.println(x);*/
+        
+        /*float v =8.9f;
+        int ar = (int) Math.floor(v);
+        System.out.println(ar);*/
+        
+        double ale = Math.random();
+        int n = (int) (0 + ale * (10-0));
+        System.out.println(n);
+    }
+}
